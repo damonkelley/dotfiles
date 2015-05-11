b1460b0d95848b3e40b141f5f80bce57a7c7e46a
Damon Kelley <damon.kelley@gmail.com>
2015-05-11 11:30:36 -0500

    Add a few aliases.

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -9,3 +9,7 @@ alias s='git status'
 # other stuff
 alias ll='ls -la --color --group-directories-first'
 alias grep='grep --color=auto'
+
+# Syntax highlighted cat - requires pygments
+alias pcat="pygmentize -f terminal256 -O style=native -g"
+alias dc="docker-compose"
