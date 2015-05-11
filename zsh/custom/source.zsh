1d4f30431c9337aa2935beb2a56f9e8c41e763a2
Damon Kelley <damon.kelley@gmail.com>
2015-05-11 11:30:55 -0500

    Source a different gruvbox file based on the OS.

--- zsh/custom/source.zsh
+++ zsh/custom/source.zsh
@@ -1,3 +1,8 @@
 # virtualenvWrapper 
 source /usr/local/bin/virtualenvwrapper.sh
-source $HOME/.vim/bundle/gruvbox/gruvbox_256palette_osx.sh
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    source $HOME/.vim/bundle/gruvbox/gruvbox_256palette_osx.sh
+else
+    source $HOME/.vim/bundle/gruvbox/gruvbox_256palette.sh
+fi
