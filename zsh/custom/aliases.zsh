6034bc80ae12c17d1bc51c7be51e6a17716c307f
Damon Kelley <damon.kelley@gmail.com>
2016-01-19 11:46:19 -0600

    Update ZSH settings.

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -6,6 +6,8 @@ alias co='git checkout'
 alias d='git diff'
 alias s='git status'
 
+alias evil='emacsclient -t'
+
 # Syntax highlighted cat - requires pygments
 alias pcat="pygmentize -f terminal256 -O style=native -g"
 alias dc="docker-compose"
--- zsh/custom/variables.zsh
+++ zsh/custom/variables.zsh
@@ -1,5 +1,11 @@
-export TERM=rxvt-unicode-256color
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    export TERM=xterm-256color
+else
+    export TERM=rxvt-unicode-256color
+fi
 export WORKON_HOME=~/.virtualenvs
 export PROJECT_HOME=~/code/
 export VIRTUAL_ENV_DISABLE_PROMPT=0
 export NVIM_TUI_ENABLE_TRUE_COLOR=1
+export ESHELL=/bin/zsh
+export PATH=/usr/local/bin:$PATH
