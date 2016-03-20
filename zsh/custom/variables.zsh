24a94bbfda6c4477dec0b05ab97b2cbc4d0d477e
Damon Kelley <damon.kelley@gmail.com>
2016-03-20 10:40:03 -0500

    Update zsh config.

--- zsh/custom/variables.zsh
+++ zsh/custom/variables.zsh
@@ -3,9 +3,11 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 else
     export TERM=rxvt-unicode-256color
 fi
+
 export WORKON_HOME=~/.virtualenvs
 export PROJECT_HOME=~/code/
 export VIRTUAL_ENV_DISABLE_PROMPT=0
 export NVIM_TUI_ENABLE_TRUE_COLOR=1
 export ESHELL=/bin/zsh
 export PATH=/usr/local/bin:$PATH
+export EDITOR=nvim
