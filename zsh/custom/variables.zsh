3823fef1ef26642700cf6ab854e9e8f64ba20e36
Damon Kelley <damon.kelley@gmail.com>
2016-05-24 23:23:30 -0500

    Remove gruvbox settings

--- zsh/custom/variables.zsh
+++ zsh/custom/variables.zsh
@@ -1,10 +1,3 @@
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    export TERM=xterm-256color
-else
-    export TERM=rxvt-unicode-256color
-fi
-
-export WORKON_HOME=~/.virtualenvs
 export PROJECT_HOME=~/code/
 export VIRTUAL_ENV_DISABLE_PROMPT=0
 export NVIM_TUI_ENABLE_TRUE_COLOR=1
