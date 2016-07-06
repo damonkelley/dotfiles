a68061f12d3b5dde14d9eda9742ed1dce5f535c1
Damon Kelley <damon.kelley@gmail.com>
2016-07-06 12:37:52 -0500

    Add script to setup of links

--- link.sh
+++ link.sh
@@ -1,1 +1,8 @@
 # Script to symlink dotfiles
+
+mkdir -p ~/.config
+ln -s ~/dotfiles/vim ~/.config/nvim 
+ln -s ~/dotfiles/idea/ideavimrc ~/.ideavimrc
+ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/fish ~/.config/fish
