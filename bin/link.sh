ccb5977bbef78980f97229f5fe76ba35701edc2f
Damon Kelley <damon.kelley@gmail.com>
2024-01-17 19:00:09 -0500

    Add a global gitignore

--- bin/link.sh
+++ bin/link.sh
@@ -8,4 +8,5 @@ ln -svf ~/dotfiles/starship/starship.toml ~/.config
 ln -svf ~/dotfiles/idea/ideavimrc ~/.ideavimrc
 ln -svf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -svf ~/dotfiles/git/gitconfig ~/.gitconfig
+ln -svf ~/dotfiles/git/gitignore_global ~/.gitignore_global
 ln -svf ~/dotfiles/zsh/zshrc ~/.zshrc
